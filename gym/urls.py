a9e86acf48a8f42c0bd400efa010429b5efb3eff
jarough <jackroughead@gmail.com>
2024-10-06 01:10:11 +0100

    Added delete functionality to equipment objects Added 'requirements.txt' file for upload on Heroku

--- gym/urls.py
+++ gym/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.EquipmentListView.as_view(), name="gym-home"),
     path('gym/create/', views.EquipmentCreateView.as_view(), name="gym-create"),
     path('gym/<int:pk>/', views.EquipmentDetailView.as_view(), name="gym-details"),
-    path('gym/<int:pk>/update', views.EquipmentUpdateView.as_view(), name="gym-update"),
+    path('gym/<int:pk>/update/', views.EquipmentUpdateView.as_view(), name="gym-update"),
+    path('gym/<int:pk>/delete/', views.EquipmentDeleteView.as_view(), name="gym-delete"),
     path('about/', views.about, name="gym-about"),
 ]
--- gym/views.py
+++ gym/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 from . import models
 
 # Create your views here.
@@ -29,13 +30,26 @@ class EquipmentCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().valid_form(form)
 
-class EquipmentUpdateView(LoginRequiredMixin, UpdateView):
+class EquipmentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = models.Equipment
     fields = "__all__"
+
+    def test_func(self):
+        gym = self.get_object()
+        return self.request.user == gym.author
 
     def valid_form(self, form):
         form.instance.author = self.request.user
         return super().valid_form(form)
 
+class EquipmentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = models.Equipment
+    success_url = reverse_lazy('gym-home')
+
+    def test_func(self):
+        gym = self.get_object()
+        return self.request.user == gym.author
+
+    
 def about(request):
     return render(request, "gym/about.html", {'title': 'About Us | FlowFitness'})
